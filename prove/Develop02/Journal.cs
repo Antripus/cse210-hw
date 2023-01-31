bf6efb4893533015d261f06599cf6f933d47a2c0
Antripus <esteban.garcia.cel@gmail.com>
2023-01-31 12:48:27 -0300

    Methods

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,12 @@
+
+
+class Journal
+{
+    public static void grabarLibro(){
+        Console.WriteLine ("HOLA");
+    }
+
+    static void grabarLibro2(){
+        Console.WriteLine ("HOLA");
+    }
+}
---
+++ prove/Develop02/JournalEntry.cs
@@ -0,0 +1,26 @@
+
+class JournalEntry
+{
+    void registrarEvento (){
+
+    }
+
+    String getPromt (){
+        
+        
+        String promt ="";
+        return promt;
+    }
+
+    static List<string> createPromts (){
+        List<string> promts = new List<string>();
+        promts.Add("What was the best part of my day?");
+        promts.Add("What was I learn today?");
+        promts.Add("Who take my attention today?");
+        promts.Add("Something surprise me today?");
+        promts.Add("How I see the hand of the lord today?");
+        
+        return promts;
+    }
+    
+}
